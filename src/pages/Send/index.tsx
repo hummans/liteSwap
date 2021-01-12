c539e5d14dc3ed9bf26e1e4024d4493950013b6e
notEternal <cent1pede@protonmail.ch>
2021-01-13 01:32:51 +0800

    Small fix

--- src/pages/Send/index.tsx
+++ src/pages/Send/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import AppBody from '../AppBody';
+// import Column, { AutoColumn } from '../../components/Column';
+// import AddressInputPanel from '../../components/AddressInputPanel';
+// import CurrencyInputPanel from '../../components/CurrencyInputPanel';
+// import Loader from '../../components/Loader';
 
 const SendWrapper = styled.section``;
 
