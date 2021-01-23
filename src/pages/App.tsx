36ac7f2bf8d4158f0ae5109394e8509e8873ef77
notEternal <cent1pede@protonmail.ch>
2021-01-23 15:05:11 +0800

    Extra global cleanup (begin from zero)

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,30 +1,15 @@
 import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
-import AddressClaimModal from '../components/claim/AddressClaimModal';
 import Header from '../components/Header';
-import Polling from '../components/Header/Polling';
 import URLWarning from '../components/Header/URLWarning';
-import Popups from '../components/Popups';
 import Web3ReactManager from '../components/Web3ReactManager';
-import { ApplicationModal } from '../state/application/actions';
-import { useModalOpen, useToggleModal } from '../state/application/hooks';
-import DarkModeQueryParamReader from '../theme/DarkModeQueryParamReader';
 import AddLiquidity from './AddLiquidity';
-import {
-  RedirectDuplicateTokenIds,
-  RedirectOldAddLiquidityPathStructure,
-  RedirectToAddLiquidity
-} from './AddLiquidity/redirects';
-import Earn from './Earn';
-import Manage from './Earn/Manage';
 import Pool from './Pool';
 import Send from './Send';
 import PoolFinder from './PoolFinder';
 import RemoveLiquidity from './RemoveLiquidity';
-import { RedirectOldRemoveLiquidityPathStructure } from './RemoveLiquidity/redirects';
 import Swap from './Swap';
-import { OpenClaimAddressModalAndRedirectToSwap, RedirectPathToSwapOnly, RedirectToSwap } from './Swap/redirects';
 
 const AppWrapper = styled.div`
   display: flex;
@@ -58,17 +43,6 @@ const BodyWrapper = styled.div`
   z-index: 1;
 `;
 
-const Marginer = styled.div`
-  margin-top: 5rem;
-`;
-
-function TopLevelModals() {
-  const open = useModalOpen(ApplicationModal.ADDRESS_CLAIM);
-  const toggle = useToggleModal(ApplicationModal.ADDRESS_CLAIM);
-
-  return <AddressClaimModal isOpen={open} onDismiss={toggle} />;
-}
-
 export default function App() {
   return (
     <Suspense fallback={null}>
@@ -80,32 +54,18 @@ export default function App() {
         </HeaderWrapper>
 
         <BodyWrapper>
-          <Popups />
-          <Polling />
-          <TopLevelModals />
           <Web3ReactManager>
             <Switch>
               <Route exact strict path="/swap" component={Swap} />
-              <Route exact strict path="/swap/:outputCurrency" component={RedirectToSwap} />
-              <Route exact strict path="/claim" component={OpenClaimAddressModalAndRedirectToSwap} />
               <Route exact strict path="/send" component={Send} />
               <Route exact strict path="/find" component={PoolFinder} />
               <Route exact strict path="/pool" component={Pool} />
-              <Route exact strict path="/luck" component={Earn} />
-              <Route exact strict path="/create" component={RedirectToAddLiquidity} />
+              <Route exact strict path="/create" component={AddLiquidity} />
               <Route exact path="/add" component={AddLiquidity} />
-              <Route exact path="/add/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
-              <Route exact path="/add/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds} />
-              <Route exact path="/create" component={AddLiquidity} />
-              <Route exact path="/create/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
-              <Route exact path="/create/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds} />
-              <Route exact strict path="/remove/:tokens" component={RedirectOldRemoveLiquidityPathStructure} />
-              <Route exact strict path="/remove/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
-              <Route exact strict path="/uni/:currencyIdA/:currencyIdB" component={Manage} />
+              <Route exact strict path="/remove" component={RemoveLiquidity} />
               <Route component={RedirectPathToSwapOnly} />
             </Switch>
           </Web3ReactManager>
-          <Marginer />
         </BodyWrapper>
       </AppWrapper>
     </Suspense>
